662d581261544270b88e9bef77d8531299bfaf3c
Garrett <xxshadowfang@gmail.com>
2015-02-11 02:42:23 -0500

    Wifi stuff

--- MyApplication/app/src/main/java/barnes/fahsl/geocatcher/ShareHuntsActivity.java
+++ MyApplication/app/src/main/java/barnes/fahsl/geocatcher/ShareHuntsActivity.java
@@ -4,6 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -15,8 +19,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ShareHuntsActivity extends ActionBarActivity {
@@ -73,6 +79,7 @@ public class ShareHuntsActivity extends ActionBarActivity {
                     Intent intent = new Intent();
                         intent.setAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
                         mReceiver.onReceive(getApplicationContext(),intent);
+                        ((WifiDirectBroadcastReceiver)mReceiver).connect();
                     }
 
                     @Override
@@ -127,6 +134,103 @@ public class ShareHuntsActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+
+        private WifiP2pManager mManager;
+        private WifiP2pManager.Channel mChannel;
+        private ShareHuntsActivity mActivity;
+        //WifiP2pManager.PeerListListener myPeerListListener;
+        private List<WifiP2pDevice> peer = new ArrayList<WifiP2pDevice>();
+
+        public WifiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                           ShareHuntsActivity activity) {
+            super();
+            this.mManager = manager;
+            this.mChannel = channel;
+            this.mActivity = activity;
+//        myPeerListListener = new WifiP2pManager.PeerListListener() {
+//            @Override
+//            public void onPeersAvailable(WifiP2pDeviceList peers) {
+//
+//            }
+//        };
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    // Wifi P2P is enabled
+                } else {
+                    // Wi-Fi P2P is not enabled
+                }
+            }
+            else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                // request available peers from the wifi p2p manager. This is an
+                // asynchronous call and the calling activity is notified with a
+                // callback on PeerListListener.onPeersAvailable()
+                if (mManager != null) {
+                    mManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
+                        @Override
+                        public void onPeersAvailable(WifiP2pDeviceList peers) {
+                            peer.clear();
+                            peer.addAll(peers.getDeviceList());
+
+                            // If an AdapterView is backed by this data, notify it
+                            // of the change.  For instance, if you have a ListView of available
+                            // peers, trigger an update.
+                            for(WifiP2pDevice wif: peer){
+                                Log.d("test",wif.toString());
+
+                            }
+                            if (peer.size() == 0) {
+                                Log.d("BARNESGL", "No devices found");
+                                return;
+                            }
+
+
+                        }
+                    });
+
+                }
+            }
+            else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+                // Respond to new connection or disconnections
+            }
+            else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+
+            }
+
+        }
+
+
+        public void connect() {
+            // Picking the first device found on the network.
+            WifiP2pDevice device = peer.get(0);
+
+            WifiP2pConfig config = new WifiP2pConfig();
+            config.deviceAddress = device.deviceAddress;
+            config.wps.setup = WpsInfo.PBC;
+
+            mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+                @Override
+                public void onSuccess() {
+                    // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+                }
+
+                @Override
+                public void onFailure(int reason) {
+                    Toast.makeText(ShareHuntsActivity.this, "Connect failed. Retry.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 }
